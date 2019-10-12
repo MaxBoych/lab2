61d88a12e6a97a161624cd68b6c6e22d701d0f4f
maxboychuk <maxboydad@gmail.com>
2019-10-12 11:15:14 +0300

    Scripted auto-commit on change (2019-10-12 11:15:14) by gitwatch.sh

---
+++ src/main/java/WordMapper.java
@@ -0,0 +1,21 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
+        StringTokenizer tokens = new StringTokenizer(value.toString().replaceAll("[^а-яА-Яa-zA-Z0-9\\s+]", "").toLowerCase());
+        while (tokens.hasMoreTokens()) {
+            Text word = new Text();
+            word.set(tokens.nextToken());
+            context.write(word, new IntWritable(1));
+        }
+    }
+}
