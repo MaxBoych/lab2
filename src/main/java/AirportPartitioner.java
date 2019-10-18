695058f04ab99c98a53f2db54d594b14080c1340
maxboychuk <maxboydad@gmail.com>
2019-10-19 01:09:51 +0300

    Scripted auto-commit on change (2019-10-19 01:09:51) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 
 public class AirportPartitioner extends HashPartitioner {
+    @Override
     public int getPartition(AirportWritableComparable key, Text value, int numReduceTasks) {
         return (Integer.valueOf(key.getAirportID()).hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
