927fbaa714f7e060d6f6a66539d638f8297a571b
maxboychuk <maxboydad@gmail.com>
2019-10-18 20:23:54 +0300

    Scripted auto-commit on change (2019-10-18 20:23:54) by gitwatch.sh

--- src/main/java/AirportWritableComparable.java
+++ src/main/java/AirportWritableComparable.java
@@ -1,3 +1,9 @@
-public class AirportWritableComparable {
-    
+public class AirportWritableComparable  {
+    private int airportID;
+    private int keyData;
+
+    public AirportWritableComparable(int airportID, int keyData) {
+        this.airportID = airportID;
+        this.keyData = keyData;
+    }
 }
