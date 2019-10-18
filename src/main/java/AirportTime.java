e2702b6543e7944bf92e6242b8ac90a4875c2ad4
maxboychuk <maxboydad@gmail.com>
2019-10-18 19:46:58 +0300

    Scripted auto-commit on change (2019-10-18 19:46:58) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -22,7 +22,15 @@ public class AirportTime {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
 
-        
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setReducerClass(JoinReducer.class);
+        job.setMapOutputKeyClass(TextPair.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
 
         /*job.setMapperClass(WordMapper.class);
         job.setReducerClass(WordReducer.class);
