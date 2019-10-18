e0158a6c3e522bb030bfad145e52f620fe4ee658
maxboychuk <maxboydad@gmail.com>
2019-10-18 20:50:27 +0300

    Scripted auto-commit on change (2019-10-18 20:50:27) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -6,20 +6,10 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-
+public class AirportMapper extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException,
-            InterruptedException {
-        StringTokenizer tokens = new StringTokenizer(value.toString().replaceAll("[^а-яА-Яa-zA-Z0-9\\s+]", "").toLowerCase());
-        while (tokens.hasMoreTokens()) {
-            Text word = new Text();
-            word.set(tokens.nextToken());
-            context.write(word, new IntWritable(1));
-            int a = 0;
-            int b = 2;
-
-
-        }
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        SystemInfo system = new SystemInfo(value);
+        context.write(new TextPair(system.getSystemCode().toString(),"0"), new Text(system.toString()));
     }
 }
