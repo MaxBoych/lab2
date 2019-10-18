c1ea4c5a2150697e1ac7a8f59b5d935a135e201f
maxboychuk <maxboydad@gmail.com>
2019-10-18 21:30:33 +0300

    Scripted auto-commit on change (2019-10-18 21:30:33) by gitwatch.sh

--- src/main/java/AirportGroupingComparator.java
+++ src/main/java/AirportGroupingComparator.java
@@ -1,5 +1,9 @@
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
+    AirportGroupingComparator() {
+        super(AirportWritableComparable.class, true);
+    }
+
     
 }
