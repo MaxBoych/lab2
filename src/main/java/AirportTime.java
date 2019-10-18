2bcc979144748db1a4c0b699de6cd1817c34e0bd
maxboychuk <maxboydad@gmail.com>
2019-10-18 21:06:04 +0300

    Scripted auto-commit on change (2019-10-18 21:06:04) by gitwatch.sh

--- src/main/java/AirportTime.java
+++ src/main/java/AirportTime.java
@@ -24,7 +24,7 @@ public class AirportTime {
 
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
 
-        job.setPartitionerClass(TextPair.FirstPartitioner.class);
+        job.setPartitionerClass();
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
         job.setReducerClass(JoinReducer.class);
         job.setMapOutputKeyClass(TextPair.class);
