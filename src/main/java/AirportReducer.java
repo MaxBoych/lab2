b01d0f2a12a07b19bdf39bd3fe352cc439075f82
maxboychuk <maxboydad@gmail.com>
2019-10-18 22:22:07 +0300

    Scripted auto-commit on change (2019-10-18 22:22:07) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -11,10 +11,23 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
         Iterator<Text> iter = values.iterator();
 
         //Text systemInfo = new Text(iter.next());
-        while (iter.hasNext()) {
-            Text call = iter.next();
+
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, summa = 0;
+
+        for (; iter.hasNext(); ) {
+            int current = Integer.parseInt(iter.next().toString());
+            if (current > max) {
+                max = current;
+            }
+            if (current < min) {
+                min = current;
+            }
+
+            summa
+
+            /*Text call = iter.next();
             Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
-            context.write(key.getFirst(), outValue);
+            context.write(key.getFirst(), outValue);*/
         }
     }
 }
