0c196c97f5a8aa841d4eeed92f2bb32eadf569d5
maxboychuk <maxboydad@gmail.com>
2019-10-19 02:39:58 +0300

    Scripted auto-commit on change (2019-10-19 02:39:58) by gitwatch.sh

--- src/main/java/FlightParserCSV.java
+++ src/main/java/FlightParserCSV.java
@@ -1,2 +1,17 @@
 public class FlightParserCSV {
+    private static final String[] EMPTY = {};
+
+    static String[] parse(String line) {
+        String[] columns = line.split(",");
+
+        for (int i = 0; i < 2; i++) {
+            String check = columns[i];
+            columns[i] = columns[i].replaceAll("\"", "");
+            if (check.equals(columns[i])) {
+                return EMPTY;
+            }
+        }
+
+        return columns;
+    }
 }
