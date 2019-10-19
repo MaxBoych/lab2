33023cf56db79724b296092eb88e8ce240630048
maxboychuk <maxboydad@gmail.com>
2019-10-19 11:17:19 +0300

    Scripted auto-commit on change (2019-10-19 11:17:19) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -21,7 +21,7 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
             //System.out.println();
             if (delay.isEmpty()) {
                 continue;
-            } else if (Integer.parseInt(delay.charAt(0)));
+            } else if (Character.getNumericValue(delay.charAt(0)));
             double current = Double.parseDouble(delay);
 
             if (current > max) {
