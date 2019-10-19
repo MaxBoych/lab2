147553687e0a372140874921e30e4c199cff98c5
maxboychuk <maxboydad@gmail.com>
2019-10-19 13:43:24 +0300

    Scripted auto-commit on change (2019-10-19 13:43:24) by gitwatch.sh

--- src/main/java/AirportGroupingComparator.java
+++ src/main/java/AirportGroupingComparator.java
@@ -11,6 +11,10 @@ public class AirportGroupingComparator extends WritableComparator {
         AirportWritableComparable a1 = (AirportWritableComparable) a;
         AirportWritableComparable b1 = (AirportWritableComparable) b;
 
-        return a1.getAirportID() - b1.getAirportID();
+        int i = a1.getAirportID() - b1.getAirportID();
+        System.out.println("Comp | a1.ID: " + a1.getAirportID() + " b1.ID: " + b1.getAirportID());
+        System.out.println(i);
+        System.out.println();
+        return i;
     }
 }
