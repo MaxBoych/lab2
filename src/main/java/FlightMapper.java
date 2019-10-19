b5e8a31a90623adb1d6cc59971ea6c46908c61fa
maxboychuk <maxboydad@gmail.com>
2019-10-19 03:16:58 +0300

    Scripted auto-commit on change (2019-10-19 03:16:58) by gitwatch.sh

--- src/main/java/FlightMapper.java
+++ src/main/java/FlightMapper.java
@@ -9,6 +9,9 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
         String[] parsed = AirportParserCSV.parse(value.toString(), 1);
+        if (parsed.length == 0) {
+            return;
+        }
         context.write(new AirportWritableComparable(Integer.parseInt(parsed[14]), 1), new Text(parsed[18]));
 
         /*ServiceCall call = new ServiceCall(value);
