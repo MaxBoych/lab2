27a9e802ef0f9fb1113051a24df43470c5c88c31
maxboychuk <maxboydad@gmail.com>
2019-10-19 12:53:02 +0300

    Scripted auto-commit on change (2019-10-19 12:53:02) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -41,7 +41,7 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
             */
         }
 
-        System.out.println("qwerty   " + count);
+        //System.out.println("qwerty   " + count);
         context.write(new Text(airportName), new Text("Max: " + max + " | Min: " + min + " | Middle: " + sum / size));
     }
 }
