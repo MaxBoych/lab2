f91791cb419a3b366b4f066bfbe8d8bc42668559
maxboychuk <maxboydad@gmail.com>
2019-10-19 10:48:59 +0300

    Scripted auto-commit on change (2019-10-19 10:48:58) by gitwatch.sh

--- src/main/java/AirportParserCSV.java
+++ src/main/java/AirportParserCSV.java
@@ -19,9 +19,13 @@ class AirportParserCSV {
             }
         } else {
             columns = line.split(",");
+            System.out.println(columns[0]);
+            System.out.println(columns[18]);
+            System.out.println(columns[19]);
             if ((columns[0].equals("YEAR")) ||
                     (columns[19].equals("1")) ||
                     (columns[18].equals("0"))) {
+                System.out.println("$$$$$$$$$$$$$$$$$$$$$$$");
                 return EMPTY;
             }
         }
